b13596878d1436c8f55727da2d0b6a62a53e66c0
Técnico em Des. de Sistemas <aluno2T1BT@educ602.local>
2022-08-19 16:40:44 -0300

    first commit

---
+++ Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Login_MVC.Areas.Identity.Data
+{
+    internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<object>
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Login_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
